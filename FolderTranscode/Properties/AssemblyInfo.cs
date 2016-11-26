43d2dedf70b1537164cecaba7f6e03e6bbfd0ffb
Jason Aquino <gitlabjason@mindtester.com>
2016-11-26 15:58:42 -0500

    AssemblyInfo.cs fix

--- FolderTranscode/Properties/AssemblyInfo.cs
+++ FolderTranscode/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FolderTranscode")]
+[assembly: AssemblyTitle("Batch Transcoder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jason Aquino")]
-[assembly: AssemblyProduct("FolderTranscode")]
+[assembly: AssemblyProduct("Batch Tanscoder")]
 [assembly: AssemblyCopyright("Copyright © 2016 Jason Aquino")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
